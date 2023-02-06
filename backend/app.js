52339f79aa7169b7c9050ec2f015fcc691a8fbe1
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-02-06 17:43:48 +0100

    Connexion de l'application à MongoDB

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,21 @@
 const express = require('express');
 // Importation de Morgan (logger HTTP)
 const morgan = require('morgan');
+// Importation de Mongoose (interface avec la base de données MongoDB)
+const mongoose = require('mongoose');
+// Importation dotenv pour les variables d'environnement
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Création de l'application Express
 const app = express();
+
+// Connexion de l'app avec la base de données MongoDB
+mongoose.connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@${process.env.CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`,
+{ useNewUrlParser: true,
+useUnifiedTopology: true})
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // Log des requests et des responses
 app.use(morgan('dev'));
