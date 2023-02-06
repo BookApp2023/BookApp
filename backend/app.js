81b7c8b458d4eba3c29c3796d99ed761dd6869ca
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-02-06 12:17:13 +0100

    Configuration Node.js/Express // Gestion des erreurs de CORS // Configuration .gitignore et .env

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+// Importation d'Express
+const express = require('express');
+// Importation de Morgan (logger HTTP)
+const morgan = require('morgan');
+
+// Création de l'application Express
+const app = express();
+
+// Log des requests et des responses
+app.use(morgan('dev'));
+
+// Gestion des erreurs de CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Middleware de test
+app.use((req, res) => {
+    res.json({message : "Votre requête a bien été reçue !"});
+});
+
+module.exports = app;
